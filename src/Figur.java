8a7ec1d2a5173047d7adeca81750333ff60ba6d5
iampb <mail@philipp-bischof.de>
2022-11-22 21:21:39 +0100

    reorganization

---
+++ src/Figur.java
@@ -0,0 +1,11 @@
+public class Figur {
+    private boolean farbe;
+
+    public Figur(boolean pFarbe) {
+        this.farbe = pFarbe;
+    }
+
+    public boolean getFarbe() {
+        return this.farbe;
+    }
+}
